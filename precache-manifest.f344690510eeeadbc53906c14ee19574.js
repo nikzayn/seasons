6527ce387448290b4db06d50c06796e276b11709
Nikhil <nikhilvaidyar1997@gmail.com>
2019-01-15 15:38:11 +0530

    Updates

---
+++ precache-manifest.f344690510eeeadbc53906c14ee19574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bf85cb5ec31a7f44998",
+    "url": "/seasons/static/css/main.13927574.chunk.css"
+  },
+  {
+    "revision": "7bf85cb5ec31a7f44998",
+    "url": "/seasons/static/js/main.7bf85cb5.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/seasons/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "b6adc54659165c3ba35f485eeeb31713",
+    "url": "/seasons/index.html"
+  }
+];
