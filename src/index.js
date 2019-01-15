448970a6be9370a9fa7a565dd71a81750dab99a0
Nikhil <nikhilvaidyar1997@gmail.com>
2019-01-15 14:58:21 +0530

    Seasons app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Season from './season';
+import Spinner from './spinner';
+
+class App extends Component{
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
+       
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }), 
+            error => this.setState({ errorMessage: error.message })
+        );
+    }
+
+    render(){
+
+        if(this.state.lat && !this.state.errorMessage){
+            return <Season season = {this.state.lat}/>
+        }  
+
+        if(!this.state.lat && this.state.errorMessage){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        return <div><Spinner /></div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/season.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './season.css';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's head to the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it is cold!",
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const Season = (props) => {
+    const seasonResult = getSeason(props.season, new Date().getMonth());
+    const {text, iconName} = seasonConfig[seasonResult];
+    return(
+        <div className={`season-display ${seasonResult}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
+};
+
+export default Season;
